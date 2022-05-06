e8c2ee68be10beb7fccd5df3c95a796b14495ebf
Allen Román <algirova@gmail.com>
2022-05-05 22:59:10 -0600

    Cuarto commit

--- Frontend/web/views.py
+++ Frontend/web/views.py
@@ -1,24 +1,8 @@
 from django.shortcuts import render
-import requests
-
-endpoint  = 'http://localhost:5000{}'
 
 # Create your views here.
 def index(request):
-    if request.method == 'GET':
-        url= endpoint.format('/Mensajes')
-        data = requests.get(url)
-        context = {
-            'data': data.text
-        }
-        return render(request, 'index.html', context)
-    elif request.method == 'POST':
-        url= endpoint.format('/Mensajes')
-        data = requests.post(url, json=request.POST)
-        context = {
-            'data': data.text
-        }
-        return render(request, 'index.html', context)
+    return render(request, 'index.html')
 
 def ayuda(request):
     return render(request, 'ayuda.html')
--- app.py
+++ app.py
@@ -30,6 +30,9 @@ def cargaMensajes():
     global listamensajes
 
     entrada = request.data.decode('utf-8')
+    trans_tab = dict.fromkeys(map(ord, u'\u0301\u0308'), None)
+    entrada = normalize('NFKC', normalize('NFKD', entrada).translate(trans_tab))
+    entrada = entrada.lower()
     xmlentrada = ET.fromstring(entrada)
     #solicitud_clasificacion = xmlentrada.getroot()
 
@@ -42,7 +45,6 @@ def cargaMensajes():
                         palabra = palabra.replace(" ", "")
                         NuevoDiccionario.positivos.append(palabra)
 
-                
                 elif subhijo.tag == "sentimientos_negativos":
                     for sentimiento in subhijo:
                         palabra = sentimiento.text
@@ -67,7 +69,7 @@ def cargaMensajes():
                                     Alias_Servicio = Alias_Servicio.replace(" ", "")
                                     NuevoServicio.alias.append(Alias_Servicio)
                                 NuevoEmpresa.servicios.append(NuevoServicio)
-                            NuevoDiccionario.empresas.append(NuevoEmpresa)
+                        NuevoDiccionario.empresas.append(NuevoEmpresa)
 
 
 
@@ -98,7 +100,7 @@ def cargaMensajes():
                             buffer += c
                             columna += 1
                             estado = 2
-                        elif c == '#':
+                        elif c == '¬':
                             print('Se aceptó la cadena!')
                             break
                         else:
@@ -138,12 +140,12 @@ def cargaMensajes():
 
                     i += 1
 
-                print(Mensaje)
                 NuevoMensaje = Mensajes(Mensaje,fecha)
                 listamensajes.append(NuevoMensaje)
     
 
     return jsonify({'Mensaje':'Se agregó exitosamente',})
+
 
 
 @app.route('/Mensajes', methods=['GET'])
@@ -157,12 +159,21 @@ def retornoXML():
         if fecha.fecha not in ListaFechas:
             ListaFechas.append(fecha.fecha)
     
-    
-
     for fecha in ListaFechas:
         PositivosTotal = NegativosTotal = NeutrosTotal = 0
         Total = 0
         print(fecha)
+        respuesta = ET.SubElement(lista_respuestas, 'respuesta')
+        fecha_respuesta = ET.SubElement(respuesta, 'fecha')
+        fecha_respuesta.text = fecha
+        mensajes = ET.SubElement(respuesta, 'mensajes')
+        
+        for empresa in NuevoDiccionario.empresas:
+            empresa.total = empresa.negativos = empresa.neutros = empresa.positivos = 0
+            for servicio in empresa.servicios:
+                servicio.total = servicio.negativos = servicio.neutros = servicio.positivos = 0
+
+
         for mensaje in listamensajes:
             if mensaje.fecha == fecha:
                 Total += 1
@@ -188,10 +199,38 @@ def retornoXML():
                     NegativosTotal += 1
                 else:
                     NeutrosTotal += 1
-                print(PositivosTotal,NegativosTotal,NeutrosTotal,Total)
+
+        print(PositivosTotal,NegativosTotal,NeutrosTotal,Total)
+        total_mensajes = ET.SubElement(mensajes, 'total')
+        positivos_mensajes = ET.SubElement(mensajes, 'positivos')
+        negativos_mensajes = ET.SubElement(mensajes, 'negativos')
+        neutros_mensajes = ET.SubElement(mensajes, 'neutros')
+
+        total_mensajes.text = str(Total)
+        positivos_mensajes.text = str(PositivosTotal)
+        negativos_mensajes.text = str(NegativosTotal)
+        neutros_mensajes.text = str(NeutrosTotal)            
+        analisis = ET.SubElement(respuesta, 'analisis')
+
+        for empresa in NuevoDiccionario.empresas:
+            print(empresa.nombre) 
+        
+        for empresa in NuevoDiccionario.empresas:
+            empresaET = ET.SubElement(analisis, 'empresa', nombre=empresa.nombre)
+            mensajesEt = ET.SubElement(empresaET, 'mensajes')
+                
+
+            for mensaje in listamensajes:
+                if mensaje.fecha == fecha:
+                    Palabras = []
+                    Palabras1 = mensaje.mensaje.replace("\n", " ")
+                    Palabras1 = Palabras1.replace("\t", " ")
+                    Palabras1 = Palabras1.replace("\r", " ")
+                    Palabras1 = Palabras1.split(" ")
+                    for palabra in Palabras1:
+                        palabra = ''.join(char for char in palabra if char.isalnum())
+                        Palabras.append(palabra)
             
-                ListaNombres = []
-                for empresa in NuevoDiccionario.empresas:
                     if empresa.nombre.lower() in Palabras:
                         empresa.total += 1
                         pos1 = neg1 = neu1 = 0
@@ -202,55 +241,288 @@ def retornoXML():
                                 neg1 += 1
                             else:
                                 pass
-                        if pos > neg:
+                        if pos1 > neg1:
                             empresa.positivos += 1
-                        elif neg > pos:
+                        elif neg1 > pos1:
                             empresa.negativos += 1
                         else:
                             empresa.neutros += 1
 
-                        print(empresa.nombre,empresa.total,empresa.positivos,empresa.negativos,empresa.neutros)
+            empresa_total = ET.SubElement(mensajesEt, 'total')
+            empresa_positivos = ET.SubElement(mensajesEt, 'positivos')
+            empresa_negativos = ET.SubElement(mensajesEt, 'negativos')
+            empresa_neutros = ET.SubElement(mensajesEt, 'neutros')
 
-                        for servicio in empresa.servicios:
-                            if servicio.nombre.lower() in Palabras:
-                                servicio.total += 1
-                                pos2 = neg2 = neu2 = 0
-                                for palabra in Palabras:
-                                    if palabra in NuevoDiccionario.positivos:
-                                        pos1 += 1
-                                    elif palabra in NuevoDiccionario.negativos:
-                                        neg1 += 1
-                                    else:
-                                        pass
-                                if pos > neg:
-                                    servicio.positivos += 1
-                                elif neg > pos:
-                                    servicio.negativos += 1
+            empresa_total.text = str(empresa.total)
+            empresa_positivos.text = str(empresa.positivos)
+            empresa_negativos.text = str(empresa.negativos)
+            empresa_neutros.text = str(empresa.neutros)
+
+            print(empresa.nombre,empresa.total,empresa.positivos,empresa.negativos,empresa.neutros)
+
+            for servicio in empresa.servicios:
+                servicioET = ET.SubElement(empresaET, 'servicio', nombre=servicio.nombre)
+                mensajeET2 = ET.SubElement(servicioET, 'mensajes')
+
+                for mensaje in listamensajes:
+                    if mensaje.fecha == fecha:
+                        Palabras = []
+                        Palabras1 = mensaje.mensaje.replace("\n", " ")
+                        Palabras1 = Palabras1.replace("\t", " ")
+                        Palabras1 = Palabras1.replace("\r", " ")
+                        Palabras1 = Palabras1.split(" ")
+                        for palabra in Palabras1:
+                            palabra = ''.join(char for char in palabra if char.isalnum())
+                            Palabras.append(palabra)
+                
+                        if servicio.nombre.lower() in Palabras:
+                            servicio.total += 1
+                            pos2 = neg2 = neu2 = 0
+                            for palabra in Palabras:
+                                if palabra in NuevoDiccionario.positivos:
+                                    pos2 += 1
+                                elif palabra in NuevoDiccionario.negativos:
+                                    neg2 += 1
                                 else:
-                                    servicio.neutros += 1
+                                    pass
+                            if pos2 > neg2:
+                                servicio.positivos += 1
+                            elif neg2 > pos2:
+                                servicio.negativos += 1
                             else:
-                                for alias in servicio.alias:
-                                    if alias.lower() in Palabras:
-                                        servicio.total += 1
-                                        pos2 = neg2 = neu2 = 0
-                                        for palabra in Palabras:
-                                            if palabra in NuevoDiccionario.positivos:
-                                                pos1 += 1
-                                            elif palabra in NuevoDiccionario.negativos:
-                                                neg1 += 1
-                                            else:
-                                                pass
-                                        if pos > neg:
-                                            servicio.positivos += 1
-                                        elif neg > pos:
-                                            servicio.negativos += 1
-                                        else:
-                                            servicio.neutros += 1
-                                    else:
-                                        pass
-                            print(servicio.nombre,servicio.total,servicio.positivos,servicio.negativos,servicio.neutros)
+                                servicio.neutros += 1
 
-    return jsonify({'Mensaje':'Se agregó exitosamente',})            
+                        else:
+                            for alias in servicio.alias:
+                                if alias.lower() in Palabras:
+                                    servicio.total += 1
+                                    pos2 = neg2 = neu2 = 0
+                                    for palabra in Palabras:
+                                        if palabra in NuevoDiccionario.positivos:
+                                            pos2 += 1
+                                        elif palabra in NuevoDiccionario.negativos:
+                                            neg2 += 1
+                                        else:
+                                            pass
+                                    if pos2 > neg2:
+                                        servicio.positivos += 1
+                                    elif neg2 > pos2:
+                                        servicio.negativos += 1
+                                    else:
+                                        servicio.neutros += 1
+                                else:
+                                    pass
+
+                servicio_total = ET.SubElement(mensajeET2, 'total')
+                servicio_positivos = ET.SubElement(mensajeET2, 'positivos')
+                servicio_negativos = ET.SubElement(mensajeET2, 'negativos')
+                servicio_neutros = ET.SubElement(mensajeET2, 'neutros')
+
+                servicio_total.text = str(servicio.total)
+                servicio_positivos.text = str(servicio.positivos)
+                servicio_negativos.text = str(servicio.negativos)
+                servicio_neutros.text = str(servicio.neutros)
+                
+                servicio.total = servicio.negativos = servicio.neutros = servicio.positivos = 0
+
+
+        
+
+
+
+
+    mydata = ET.tostring(lista_respuestas)
+    myfile = open('ListasRespuestas.xml','wb')
+    myfile.write(mydata)                  
+
+    return mydata            
+
+@app.route('/Peticiones/MensajePrueba', methods=['POST'])
+def MensajePrueba():
+    global NuevoDiccionario
+    global listamensajes
+    global mydata
+
+    entrada = request.data.decode('utf-8')
+    trans_tab = dict.fromkeys(map(ord, u'\u0301\u0308'), None)
+    entrada = normalize('NFKC', normalize('NFKD', entrada).translate(trans_tab))
+    entrada = entrada.lower()
+    xmlentrada = ET.fromstring(entrada)
+    Mensaje=""
+    Variables = []
+    
+    linea = 1
+    columna = 1
+    centinela = '¬'
+    buffer = ''
+    estado = 0
+    fecha = ""
+    aux = ""
+
+    Mensaje = xmlentrada.text
+        
+    Mensaje += centinela
+    i = 0
+    print(Mensaje)
+
+    while i < len(Mensaje):
+        c = Mensaje[i]
+        if estado == 0:
+            if c.isalpha():
+                columna += 1
+                estado = 1
+            elif c.isdigit():
+                buffer += c
+                columna += 1
+                estado = 2
+            elif c == '¬':
+                break
+            else:
+                buffer += c
+                buffer = ''
+                columna += 1
+
+        elif estado == 1:
+            if c.isalpha():
+                linea += 1
+                columna = 1
+            elif c == ',':
+                linea += 1
+                columna = 1
+            elif c == ' ':
+                linea += 1
+                columna = 1
+                estado = 0
+            elif c == ':':
+                columna += 1
+                estado = 4
+                i+=1
+            else:
+                columna += 1
+
+        elif estado == 2:
+            if c.isdigit():
+                buffer += c
+                linea += 1
+                columna = 1
+            elif c == "/":
+                buffer += c
+                linea += 1
+                columna = 1
+            elif c == ' ':
+                fecha += buffer
+                buffer = ''
+                estado = 3
+            else:
+                pass
+
+        elif estado == 3:
+            buffer += c
+            estado = 0
+
+        elif estado == 4:
+            
+            if c == ' ':
+                estado = 0
+                Variables.append(buffer)
+                buffer =''
+            else:
+                buffer += c
+
+        i += 1
+        print("Carac: "+c+" est: "+str(estado))
+
+    print(Variables)
+    respuesta = ET.Element('respuesta')
+    fecha_respuesta = ET.SubElement(respuesta, 'fecha')
+    fecha_respuesta.text = fecha
+
+    red_social = ET.SubElement(respuesta, 'red_social')
+    red_social.text = Variables[2]
+
+    usuario = ET.SubElement(respuesta, 'usuario')
+    usuario.text = Variables[1]
+
+    
+    empresasEt = ET.SubElement(respuesta, 'empresas')
+    pos = neg = 0
+    Palabras = []
+    Palabras1 = Mensaje.replace("\n", " ")
+    Palabras1 = Palabras1.replace("\t", " ")
+    Palabras1 = Palabras1.replace("\r", " ")
+    Palabras1 = Palabras1.split(" ")
+    for palabra in Palabras1:
+        palabra = ''.join(char for char in palabra if char.isalnum())
+        Palabras.append(palabra)
+    
+    for empresa in NuevoDiccionario.empresas:
+        if empresa.nombre.lower() in Palabras:
+            empresaET = ET.SubElement(empresasEt, 'empresa')
+            empresasEt.text = empresa.nombre
+
+            for servicio in empresa.servicios:
+                if servicio.nombre.lower() in Palabras:
+                    servicioET = ET.SubElement(empresaET, 'servicio')
+                    servicioET.text = servicio.nombre
+                else:
+                    for alias in servicio.alias:
+                        if alias.lower() in Palabras:
+                            servicioET = ET.SubElement(empresaET, 'servicio')
+                            servicioET.text = servicio.nombre
+                        else:
+                            pass
+    
+    Palabras1 = Mensaje.replace("\n", " ")
+    Palabras1 = Palabras1.replace("\t", " ")
+    Palabras1 = Palabras1.replace("\r", " ")
+    Palabras1 = Palabras1.split(" ")
+    for palabra in Palabras1:
+        palabra = ''.join(char for char in palabra if char.isalnum())
+        Palabras.append(palabra)
+        
+    for palabra in Palabras:
+        if palabra in NuevoDiccionario.positivos:
+            pos += 1
+        elif palabra in NuevoDiccionario.negativos:
+            neg += 1
+        else:
+            pass
+    
+    palabras_positivas = ET.SubElement(respuesta, 'palabras_positivas')
+    palabras_positivas.text = str(pos)
+
+    palabras_negativas = ET.SubElement(respuesta, 'palabras_negativas')
+    palabras_negativas.text = str(neg)
+
+    sentimiento_positivo = ET.SubElement(respuesta, 'sentimiento_positivo')
+    sentimiento_positivo.text = str((pos/(pos+neg))*100)+"%"
+
+    sentimiento_negativo = ET.SubElement(respuesta, 'sentimiento_negativo')
+    sentimiento_negativo.text = str((neg/(pos+neg))*100)+"%"
+
+    sentimiento_analizado = ET.SubElement(respuesta, 'sentimiento_analizado')
+    if pos > neg:
+        sentimiento_analizado.text = "Positivo"
+    elif neg > pos:
+        sentimiento_analizado.text = "Negativo"
+    else:
+        sentimiento_analizado.text = "Neutro"
+
+    mydata = ET.tostring(respuesta)
+    myfile = open('Mensaje.xml','wb')
+    myfile.write(mydata)                  
+
+    return jsonify({'Mensaje':'Se agregó exitosamente',}) 
+
+@app.route('/Peticiones/MensajePrueba', methods=['GET'])
+def MensajePruebaRet():
+    global mydata
+
+    return mydata
+
+
+    
+
 
                 
 
